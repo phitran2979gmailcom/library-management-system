aac2484bb25e0411771758b49b0f8b452db44d60
missentrovie <helbryna@yandex.com>
2026-01-16 02:42:48 +0300

    Update 03_views.sql

--- sql/03_views.sql
+++ sql/03_views.sql
@@ -9,6 +9,7 @@ JOIN members m ON l.member_ID = m.member_ID
 JOIN books b ON l.book_ID = b.book_ID
 WHERE l.return_date IS NULL;
 
+
 CREATE VIEW overdue_loans AS
 SELECT
     m.full_name AS member,
@@ -20,11 +21,13 @@ JOIN members m ON l.member_ID = m.member_ID
 JOIN books b ON l.book_ID = b.book_ID
 WHERE l.return_date IS NULL AND l.due_date < CURRENT_DATE;
 
+
 CREATE VIEW popular_books AS
-SELECT
-    b.title,
+SELECT b.title,
     COUNT(l.loan_ID) AS borrow_count
 FROM books b
 LEFT JOIN loans l ON b.book_ID = l.book_ID
 GROUP BY b.book_ID, b.title
-ORDER BY borrow_count DESC;
+ORDER BY borrow_count DESC
+LIMIT 10;
+
