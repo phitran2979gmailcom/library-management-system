b7c4c58de649ba84c3a324a055d69c1ec45ef0eb
missentrovie <helbryna@yandex.com>
2026-01-16 01:54:35 +0300

    Add triggers to update stock on loan and return

--- sql/02_triggers.sql
+++ sql/02_triggers.sql
@@ -1,1 +1,33 @@
+CREATE OR REPLACE FUNCTION update_stock_on_loan ()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF (SELECT available_copies FROM books
+		WHERE book_ID = NEW.book_ID) <= 0 
+		THEN
+        RAISE EXCEPTION 'No available copies for book ID %', NEW.book_ID;
+    END IF;
+    UPDATE books SET available_copies = available_copies - 1
+    WHERE book_ID = NEW.book_ID;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
+CREATE TRIGGER trg_update_stock_on_loan
+BEFORE INSERT ON loans
+FOR EACH ROW EXECUTE FUNCTION update_stock_on_loan();
+
+CREATE OR REPLACE FUNCTION update_stock_on_return()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF OLD.return_date IS NULL AND NEW.return_date IS NOT NULL 
+		THEN
+        UPDATE books SET available_copies = available_copies + 1
+        WHERE book_ID = NEW.book_ID;
+    END IF;
+    RETURN NEW;
+	END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_update_stock_on_return
+BEFORE UPDATE ON loans
+FOR EACH ROW EXECUTE FUNCTION update_stock_on_return();
